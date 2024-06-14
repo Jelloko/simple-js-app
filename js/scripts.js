9c6535a76d992c88f640be816eb9dcd590fe5abe
Jelloko <rainbow.samp@gmail.com>
2024-06-14 02:09:10 -0400

    this commit should add a for loop that lists the pokemon and height from pokemonList also adding the words "Wow, that's big!" beside the largest height. Lastly it should add a </br> for a line break.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 let pokemonList = [
-    { pokemon: 'Nidoran', height: '0.5m', type: 'Poison'}
-    { pokemon: 'Poliwag', height: '0.6m', type: 'Water'}
-    { pokemon: 'Voltorb', height: '0.5m', type: 'Electric'}
-]
+    { pokemon: 'Nidoran', height: 0.5, type: 'Poison'},
+    { pokemon: 'Poliwag', height: 0.6, type: 'Water'},
+    { pokemon: 'Voltorb', height: 0.5, type: 'Electric'}
+];
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokel = pokemonList[i];
+    document.write(pokel.pokemon + " (height: " + pokel.height + ")");
+    if (pokel.height > 0.5) {
+    document.write("-Wow, that's big!")
+    }
+    document.write("</br>")
+};
+
