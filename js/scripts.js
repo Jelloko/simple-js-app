8a5aba08d74b19cff03c25e70bb3bb980d04db66
Jelloko <rainbow.samp@gmail.com>
2024-06-25 00:16:10 -0400

    This commit should replace the for loop with a forEach loop.

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,11 @@ let pokemonList = [
     { pokemon: 'Nidoran', height: 0.5, type: 'Poison'},
     { pokemon: 'Poliwag', height: 0.6, type: 'Water'},
     { pokemon: 'Voltorb', height: 0.5, type: 'Electric'}
-];
-for (let i = 0; i < pokemonList.length; i++) {
-    let pokel = pokemonList[i];
-    document.write(pokel.pokemon + " (height: " + pokel.height + ")");
-    if (pokel.height > 0.5) {
-    document.write("-Wow, that's big!")
+  ];
+  pokemonList.forEach(function(item) {
+    document.write(item.pokemon + " (height: " + item.height + ")");
+    if (item.height > 0.5) {
+      document.write("-Wow, that's big!");
     }
-    document.write("</br>")
-};
-
+    document.write("</br>");
+  });
