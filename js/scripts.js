b2d857612369c1db0ee7b0219d9ff580d944734f
Jelloko <rainbow.samp@gmail.com>
2024-06-25 01:19:45 -0400

    This commit should add an IIFE "pokemonRepository" for the "pokemonList" array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+ let pokemonList = [
     { pokemon: 'Nidoran', height: 0.5, type: 'Poison'},
     { pokemon: 'Poliwag', height: 0.6, type: 'Water'},
     { pokemon: 'Voltorb', height: 0.5, type: 'Electric'}
   ];
-  pokemonList.forEach(function(item) {
-    document.write(item.pokemon + " (height: " + item.height + ")");
-    if (item.height > 0.5) {
-      document.write("-Wow, that's big!");
-    }
-    document.write("</br>");
-  });
+
+  function getAll () {
+    return pokemonList;
+  }
+  function add (item) {
+    pokemonList.push(item);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+
+})();
+  
+pokemonRepository.getAll().forEach(function(item) {
+ document.write(item.pokemon + " (height: " + item.height + ")");
+if (item.height > 0.5) {
+ document.write("-Wow, that's big!");
+}
+ document.write("</br>");
+});
