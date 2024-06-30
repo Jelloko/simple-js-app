0b992031c3fc61f21524b315b8eff836659ee767
Jelloko <rainbow.samp@gmail.com>
2024-06-29 23:17:58 -0400

    This commit should add the functions 'addListItem' and 'showDetails' . This commit should also delete the previous code in the forEach loop and replace it with the 'addListItem' function
    This commit also added comments to the scripts.js page

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,50 @@
+// Array of Pokemon's pokemon,heights and types
 let pokemonRepository = (function () {
- let pokemonList = [
-    { pokemon: 'Nidoran', height: 0.5, type: 'Poison'},
-    { pokemon: 'Poliwag', height: 0.6, type: 'Water'},
-    { pokemon: 'Voltorb', height: 0.5, type: 'Electric'}
+  let pokemonList = [
+     { pokemon: 'Nidoran', height: 0.5, type: 'Poison'},
+     { pokemon: 'Poliwag', height: 0.6, type: 'Water'},
+     { pokemon: 'Voltorb', height: 0.5, type: 'Electric'}
   ];
 
-  function getAll () {
-    return pokemonList;
-  }
-  function add (item) {
-    pokemonList.push(item);
+// Function to return everything in the array
+   function getAll () {
+     return pokemonList;
   }
 
-  return {
-    getAll: getAll,
-    add: add
+// Function to add to the array
+   function add (item) {
+     pokemonList.push(item);
+  }
+
+// function to showdetails of pokemon
+   function showDetails(item){
+    console.log(item.pokemon)
+  }
+
+// Function to make a list of buttons for pokemon and show details
+   function addListItem(item){
+     let pokelisty = document.querySelector(".pokemon-list");
+     let itemlist = document.createElement('li');
+     let button = document.createElement('button');
+     button.innerText = item.pokemon;
+     button.classList.add("button-class");
+     itemlist.appendChild(button);
+     pokelisty.appendChild(itemlist);
+     button.addEventListener('click', function (event) {
+     pokemonRepository.showDetails(item);
+  });
+  }
+
+// All possible returns for functions
+    return {
+     getAll: getAll,
+     add: add,
+     addListItem: addListItem,
+     showDetails: showDetails
   };
 
-})();
-  
-pokemonRepository.getAll().forEach(function(item) {
- document.write(item.pokemon + " (height: " + item.height + ")");
-if (item.height > 0.5) {
- document.write("-Wow, that's big!");
-}
- document.write("</br>");
-});
+  })();
+// Returning function getAll and addListItem to make a list of buttons forEach pokemon
+ pokemonRepository.getAll().forEach(function(item) {
+    pokemonRepository.addListItem(item)
+  });
